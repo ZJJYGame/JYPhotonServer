e2915714d764d5bf4f2df2116d0457f3a30c8ddd
Don <zhengdonn@gmail.com>
2020-04-14 21:06:45 +0800

    Update protocol
    Update protocol

---
+++ GameServer/AscensionProtocol/Model/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AscensionProtocol.Model
+{
+    public class User
+    {
+        public virtual int ID { get; set; }
+        public virtual string Username { get; set; }
+        public virtual string Password { get; set; }
+        public virtual string UUID { get; set; }
+    }
+}
