4b2bbffdc6a44d4420d694a963f0fbc52ce2e177
Don <zhengdonn@gmail.com>
2020-04-17 21:01:08 +0800

    Update server
    Update server

--- GameServer/AscensionServer/Handler/IHandler.cs
+++ GameServer/AscensionServer/Handler/IHandler.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿/*
+*Author   Don
+*Since 	2020-04-17
+*Description 消息处理接口
+*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AscensionProtocol;
 using Photon.SocketServer;
+
 namespace AscensionServer
 {
     public interface IHandler
