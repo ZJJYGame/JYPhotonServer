7d848b20c765985e317f4498a516ae67d6299a9a
Zt01221510 <49228668+Zt01221510@users.noreply.github.com>
2020-04-22 11:53:26 +0800

    Update Registrer Server
    Update Registrer Server

--- GameServer/AscensionServer/Handler/RegisterHandler.cs
+++ GameServer/AscensionServer/Handler/RegisterHandler.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Photon.SocketServer;
+using AscensionProtocol;
+using AscensionServer.Model;
 
 namespace AscensionServer
 {
@@ -16,6 +18,26 @@ namespace AscensionServer
 
         public override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters, ClientPeer peer)
         {
+            string username = Utility.GetValue<byte, object>(operationRequest.Parameters, (byte)ParameterCode.Username) as string;
+            string password = Utility.GetValue<byte, object>(operationRequest.Parameters, (byte)ParameterCode.Password) as string;
+            UserManager manager = new UserManager();
+            User user = manager.GetByUsername(username);//根据username查询数据
+            OperationResponse responser = new OperationResponse(operationRequest.OperationCode);
+            //如果没有查询到代表这个用户没被注册过可用
+            if (user == null)
+            {
+                //添加输入的用户和密码进数据库
+                user = new User() { Username = username, Password = password };
+                manager.Add(user);
+                responser.ReturnCode = (short)ReturnCode.Success;//返回成功
+
+            }
+            else//否者这个用户被注册了
+            {
+                responser.ReturnCode = (short)ReturnCode.Fail;//返回失败
+            }
+            // 把上面的结果给客户端
+            peer.SendOperationResponse(responser, sendParameters);
         }
     }
 }
