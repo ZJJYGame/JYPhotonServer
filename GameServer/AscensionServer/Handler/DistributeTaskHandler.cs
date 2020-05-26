b54b2777ce2456ed767fed4e294ef4ec1b9e8c0f
Zt01221510 <49228668+Zt01221510@users.noreply.github.com>
2020-05-26 10:16:15 +0800

    edit distributetask

--- GameServer/AscensionServer/Handler/DistributeTaskHandler.cs
+++ GameServer/AscensionServer/Handler/DistributeTaskHandler.cs
@@ -8,6 +8,7 @@ using Photon.SocketServer;
 using AscensionProtocol;
 using AscensionProtocol.DTO;
 using AscensionServer.Model;
+using System.Collections.Generic;
 
 namespace AscensionServer.Handler
 {
@@ -21,10 +22,26 @@ namespace AscensionServer.Handler
         public override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters, AscensionPeer peer)
         {
             string roletask = Convert.ToString(Utility.GetValue(operationRequest.Parameters,(byte)ObjectParameterCode.Role));
+            AscensionServer.log.Info(">>>>>>>>>>>>>接受到的任务相关信息："+roletask+">>>>>>>>>>>>>>>>>>>>>>");
             var roletaskobj = Utility.ToObject<DistributeTaskDTO>(roletask);
+            NHCriteria nHCriteriaTask = Singleton<ReferencePoolManager>.Instance.Spawn<NHCriteria>().SetValue("RoleID", roletaskobj.RoleID);
+            bool exist = Singleton<NHManager>.Instance.Verify<Role>(nHCriteriaTask);
+            OperationResponse operationResponse = new OperationResponse(operationRequest.OperationCode);
+            if (exist)
+            {
+                Singleton<NHManager>.Instance.Update(roletaskobj);
+                RoleTaskProgress roleTaskProgress = Singleton<NHManager>.Instance.CriteriaGet<RoleTaskProgress>(nHCriteriaTask);
+                AscensionServer.log.Info(">>>>>>>>>>>>传回去的" + roleTaskProgress + ">>>>>>>>>>>>");
+                Dictionary<byte, object> data = new Dictionary<byte, object>();
+                data.Add((byte)ObjectParameterCode.Role, roleTaskProgress);
+                operationResponse.Parameters = data;
+                operationResponse.ReturnCode = (short)ReturnCode.Success;
 
-
-
+            }
+            else
+                operationResponse.ReturnCode = (short)ReturnCode.Fail;
+            peer.SendOperationResponse(operationResponse,sendParameters);
+            Singleton<ReferencePoolManager>.Instance.Despawn(nHCriteriaTask);
         }
        
 
