b0b3b24968ab740ed2e5278dd9067f3ef42ba4e0
Zt01221510 <49228668+Zt01221510@users.noreply.github.com>
2020-05-27 14:23:17 +0800

    editroleassetsDTO

---
+++ GameServer/AscensionProtocol/DTO/RoleAssetsDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AscensionProtocol;
+
+namespace AscensionProtocol.DTO
+{
+    [Serializable]
+   public class RoleAssetsDTO:ProtocolDTO
+    {
+        public virtual int RoleID { get; set; }
+        public virtual int SpiritStonesLow { get; set; }
+        public virtual int SpiritStonesMedium { get; set; }
+        public virtual int SpiritStonesHigh { get; set; }
+        public virtual int XianYu { get; set; }
+    }
+}
