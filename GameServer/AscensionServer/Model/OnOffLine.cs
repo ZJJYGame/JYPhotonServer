7da1688aa473e1b5b9f393c627b0ce36feffc101
英短 <39549944+nqclymt@users.noreply.github.com>
2020-06-02 15:45:13 +0800

    update task
    update task

---
+++ GameServer/AscensionServer/Model/OnOffLine.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AscensionServer.Model
+{
+    [Serializable]
+    public class OnOffLine 
+    {
+        public virtual int RoleID { get; set; }
+        public virtual string OnLineTime { get; set; }
+        public virtual string OffLineTime { get; set; }
+    }
+}
---
+++ GameServer/AscensionServer/ModelMap/OnOffLineMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentNHibernate.Mapping;
+using AscensionServer.Model;
+
+namespace AscensionServer.ModelMap
+{
+    public class OnOffLineMap : ClassMap<OnOffLine>
+    {
+        public OnOffLineMap()
+        {
+            Id(x => x.RoleID).GeneratedBy.Increment().Column("roleid");
+            Map(x => x.OnLineTime).Column("onlinetime");
+            Map(x => x.OffLineTime).Column("offlinetime");
+            Table("onoffline");
+        }
+    }
+}
