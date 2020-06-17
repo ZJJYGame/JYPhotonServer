7c17b070b1826513f594832bdb1dfcd9a2411442
Zt01221510 <49228668+Zt01221510@users.noreply.github.com>
2020-06-17 11:42:20 +0800

    edit removepet

---
+++ GameServer/AscensionServer/Handlers/SyncPetStatus/SubHandler/RemovePetStatusSubHandler.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AscensionProtocol;
+using Photon.SocketServer;
+using AscensionServer.Model;
+using Cosmos;
+
+namespace AscensionServer.Handlers.SyncPetStatus.SubHandler
+{
+
+    public class RemovePetStatusSubHandler : SyncPetStatusSubHandler
+    {
+        public override void OnInitialization()
+        {
+            SubOpCode = SubOperationCode.Remove;
+            base.OnInitialization();
+        }
+        public override void Handler(OperationRequest operationRequest, SendParameters sendParameters, AscensionPeer peer)
+        {
+        var dict = ParseSubDict(operationRequest);
+            string petstatus = Convert.ToString(Utility.GetValue(dict,(byte)ObjectParameterCode.PetStatus));
+        }
+    }
+}
