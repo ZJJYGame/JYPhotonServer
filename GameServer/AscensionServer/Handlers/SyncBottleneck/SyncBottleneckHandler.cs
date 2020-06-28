bcfc3678a5cf8f4280ecfe585cd578b8da4300e6
Zt01221510 <49228668+Zt01221510@users.noreply.github.com>
2020-06-28 17:16:26 +0800

    edit

--- GameServer/AscensionServer/Handlers/SyncBottleneck/SyncBottleneckHandler.cs
+++ GameServer/AscensionServer/Handlers/SyncBottleneck/SyncBottleneckHandler.cs
@@ -18,20 +18,24 @@ namespace AscensionServer
         }
         public override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters, AscensionPeer peer)
         {
-            string roleJson=Convert.ToString(Utility.GetValue(operationRequest.Parameters, (byte)ParameterCode.RoleBottleneck));
+            string roleJson = Convert.ToString(Utility.GetValue(operationRequest.Parameters, (byte)ParameterCode.RoleBottleneck));
+           // AscensionServer._Log.Info(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>传过来的瓶颈状态" + roleJson);
             var bottleneckObj = Utility.Json.ToObject<Bottleneck>(roleJson);
             NHCriteria nHCriteriaBottleneck = Singleton<ReferencePoolManager>.Instance.Spawn<NHCriteria>().SetValue("RoleID", bottleneckObj.RoleID);
-          
+
+
             Bottleneck bottleneck = Singleton<NHManager>.Instance.CriteriaSelect<Bottleneck>(nHCriteriaBottleneck);
-            if (bottleneck!=null)
+            if (bottleneck != null)
             {
                 Singleton<NHManager>.Instance.Update(bottleneckObj);
+                OpResponse.ReturnCode = (short)ReturnCode.Success;
             }
             else
             {
                 Singleton<NHManager>.Instance.Insert(bottleneckObj);
+                OpResponse.ReturnCode = (short)ReturnCode.Success;
             }
-            //peer.SendOperationResponse(OpResponse, sendParameters);
+            peer.SendOperationResponse(OpResponse, sendParameters);
             Singleton<ReferencePoolManager>.Instance.Despawns(nHCriteriaBottleneck);
         }
     }
