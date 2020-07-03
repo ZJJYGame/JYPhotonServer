fc0110613c947adf892d477ee21706c30e6a6d97
英短 <39549944+nqclymt@users.noreply.github.com>
2020-07-03 15:01:09 +0800

    update‘  move status
    update‘  move status

--- GameServer/AscensionServer/Handlers/SyncMoveStatus/SyncMoveStatusHandler.cs
+++ GameServer/AscensionServer/Handlers/SyncMoveStatus/SyncMoveStatusHandler.cs
@@ -7,6 +7,9 @@ using AscensionProtocol;
 using Photon.SocketServer;
 using System;
 using Cosmos;
+using AscensionProtocol.DTO;
+using AscensionServer.Threads;
+
 namespace AscensionServer
 {
 
@@ -20,9 +23,26 @@ namespace AscensionServer
         //获取客户端玩家移动状态请求的处理的代码
         public override void OnOperationRequest(OperationRequest operationRequest, SendParameters sendParameters, AscensionPeer peer)
         {
+            ResponseData.Clear();
             var roleMoveStatusJson = Convert.ToString(Utility.GetValue(operationRequest.Parameters, (byte)ParameterCode.RoleMoveStatus));
             peer.RoleMoveStatusJson = roleMoveStatusJson;
+            peer.RoleMoveStatus = Utility.Json.ToObject<RoleMoveStatusDTO>(roleMoveStatusJson);
             AscensionServer._Log.Info("Role:ID " + peer.PeerCache.RoleID + "\n RoleJson :" + roleMoveStatusJson);
+            OpResponse.OperationCode = operationRequest.OperationCode;
+            OpResponse.ReturnCode = (short)ReturnCode.Success;
+            peer.SendOperationResponse(OpResponse, sendParameters);
+        
+            var peerSet =  AscensionServer.Instance.AdventureScenePeerCache.GetValuesList();
+            var threadData = Singleton<ReferencePoolManager>.Instance.Spawn<ThreadData<AscensionPeer>>();
+            threadData.SetData(peerSet, (byte)EventCode.SyncRoleMoveStatus, peer);
+            var syncEvent = Singleton<ReferencePoolManager>.Instance.Spawn<SyncRoleMoveStatusEvent>();
+            syncEvent.SetData(threadData);
+            syncEvent.AddFinishedHandler(() => {
+                Singleton<ReferencePoolManager>.Instance.Despawns(syncEvent, threadData);
+                ThreadEvent.RemoveSyncEvent(syncEvent);
+            });
+            ThreadEvent.AddSyncEvent(syncEvent);
+            ThreadEvent.ExecuteEvent();
         }
     }
 }
