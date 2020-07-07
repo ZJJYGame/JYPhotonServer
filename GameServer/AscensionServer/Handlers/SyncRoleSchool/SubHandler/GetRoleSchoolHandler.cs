04e4fec1bc2c9149de48e1b5bc139b4406cb625a
Zt01221510 <49228668+Zt01221510@users.noreply.github.com>
2020-07-07 17:14:31 +0800

    edit

--- GameServer/AscensionServer/Handlers/SyncRoleSchool/SubHandler/GetRoleSchoolHandler.cs
+++ GameServer/AscensionServer/Handlers/SyncRoleSchool/SubHandler/GetRoleSchoolHandler.cs
@@ -26,11 +26,11 @@ namespace AscensionServer
             var dict = ParseSubDict(operationRequest);
             string roleListJson = Convert.ToString(Utility.GetValue(dict, (byte)ParameterCode.RoleSchool));
             var roleobj = Utility.Json.ToObject<List<int>>(roleListJson);
-            Dictionary<int, School> schoolDict;
+            Dictionary<int, School> schoolDict = new Dictionary<int, School>();
             if (roleobj.Count>0)
             {
                 AscensionServer._Log.Info(">>>>>>>>>>>>>>>>>>>>>>>>..收到请求宗门的请求"+ roleListJson);
-                schoolDict = new Dictionary<int, School>();
+
                 foreach (var roleId in roleobj)
                 {
                     NHCriteria nHCriteriarole = Singleton<ReferencePoolManager>.Instance.Spawn<NHCriteria>().SetValue("RoleID", roleId);
@@ -38,15 +38,16 @@ namespace AscensionServer
                     if (!string.IsNullOrEmpty(roleschoolObj.RoleJoiningSchool))
                     {
                         AscensionServer._Log.Info(">>>>>>>>>>>>>>>>>>>>>>>>..收到请求宗门的请求2");
-                        foreach (var item in Utility.Json.ToObject<Dictionary<int,int>>(roleschoolObj.RoleJoiningSchool))
+                        foreach (var item in Utility.Json.ToObject<Dictionary<int, int>>(roleschoolObj.RoleJoiningSchool))
                         {
                             NHCriteria nHCriteriaSchool = Singleton<ReferencePoolManager>.Instance.Spawn<NHCriteria>().SetValue("ID", item.Key);
                             var schoolObj = Singleton<NHManager>.Instance.CriteriaSelect<School>(nHCriteriaSchool);
                             schoolDict.Add(roleId, schoolObj);
                             Singleton<ReferencePoolManager>.Instance.Despawns(nHCriteriaSchool);
                         }
-                       
+
                     }
+                 
                     Singleton<ReferencePoolManager>.Instance.Despawns(nHCriteriarole);
                 }
                 SetResponseData(() =>
@@ -57,15 +58,7 @@ namespace AscensionServer
                     Owner.OpResponse.ReturnCode = (byte)ReturnCode.Success;
                 });
             }
-            else
-            {
-                SetResponseData(() =>
-                {
-
-                    Owner.OpResponse.ReturnCode = (byte)ReturnCode.Fail;
-                });
-            }
-
+            
             peer.SendOperationResponse(Owner.OpResponse, sendParameters);
 
         }
