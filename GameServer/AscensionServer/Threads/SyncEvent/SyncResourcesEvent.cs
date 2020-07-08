159915a3b2f946b8ac8dc3f2d9e90afc017c910a
英短 <39549944+nqclymt@users.noreply.github.com>
2020-07-08 10:33:41 +0800

    res
    res

--- GameServer/AscensionServer/Threads/SyncEvent/SyncResourcesEvent.cs
+++ GameServer/AscensionServer/Threads/SyncEvent/SyncResourcesEvent.cs
@@ -1,13 +1,36 @@
-﻿
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Cosmos;
+using AscensionProtocol;
 
 namespace AscensionServer.Threads
 {
-    class SyncResourcesEvent
+    public class SyncResourcesEvent : SyncEvent
     {
+        ThreadData<AscensionPeer> threadData;
+        public override void Handler(object state)
+        {
+            if (threadData != null)
+            {
+                int peerCount = threadData.Collection.Count;
+                var roleJson = Utility.Json.ToJson(threadData.Data.PeerCache.Resources);
+                EventDataDict.Clear();
+                EventDataDict.Add((byte)ParameterCode.SyncResourcesLoad, roleJson);
+                EventData.Parameters = EventDataDict;
+                EventData.Code = threadData.EventCode;
+                foreach (var peer in threadData.Collection)
+                {
+                    peer.SendEvent(EventData, SendParameter);
+                }
+            }
+            finishedHandler?.Invoke();
+        }
+        public override void SetData(IThreadData eventArgs)
+        {
+            threadData = eventArgs as ThreadData<AscensionPeer>;
+        }
     }
 }
