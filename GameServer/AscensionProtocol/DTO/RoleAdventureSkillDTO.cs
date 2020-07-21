2b7448a8c84bb422830c4eedc44fa1ed25d4c3f9
Zt01221510 <49228668+Zt01221510@users.noreply.github.com>
2020-07-21 17:42:13 +0800

    edit

--- GameServer/AscensionProtocol/DTO/RoleAdventureSkillDTO.cs
+++ GameServer/AscensionProtocol/DTO/RoleAdventureSkillDTO.cs
@@ -13,7 +13,7 @@ namespace AscensionProtocol.DTO
         public int SkillID  { get; set; }
         public int CDInterval { get; set; }
         public int BuffeInterval { get; set; }
-        public FeatureSkillTypeEnum featureSkillTypeEnum { get; set; }
+
         public override void Clear()
         {
             RoleID = -1;
@@ -21,31 +21,8 @@ namespace AscensionProtocol.DTO
             SkillID = 0;
             CDInterval = 0;
             BuffeInterval = 0;
+
         }
-        /// <summary>
-        /// 无技能使用
-        /// 远遁
-        /// 加速移动
-        /// 隐匿
-        /// 查看周围隐匿单位
-        /// 破除隐匿
-        /// 闪现
-        /// </summary>
-        public enum FeatureSkillTypeEnum : short
-        {
-            Zero = 0,
-            Tp = 2230,
-            SpeetMove = 2231,
-            Stealth = 2232,
-            Visible = 2233,
-            Eradicate = 2234,
-            FlashBeforeOne = 2235
-            //    0,
-            //2230,
-            //2231,
-            //2232,
-            //2233,
-            //2234
-        }
+       
     }
 }
