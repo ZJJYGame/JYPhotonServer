b2f390bb5ef58d51270b863db25af1412c09a2ab
Don <zhengdonn@gmail.com>
2020-08-17 21:00:20 +0800

    Update Async

---
+++ GameServer/AscensionServer/Ascension/GameCore/Modules/SceneManager/SceneManager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmos;
+
+namespace AscensionServer
+{
+    public class SceneManager:ModuleBase<SceneManager>
+    {
+        /// <summary>
+        /// 进入场景
+        /// </summary>
+        /// <param name="sceneIndex">场景ID</param>
+        /// <param name="id">actorID</param>
+        public void EnterScene(short sceneIndex,int id)
+        {
+
+        }
+    }
+}
