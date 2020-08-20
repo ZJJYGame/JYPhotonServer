e46ba42d4257ce49d4f60b6bdc78fea7be86159a
Zt01221510 <39549944+nqclymt@users.noreply.github.com>
2020-08-20 14:15:42 +0800

    res
    res

--- GameServer/AscensionServer/Ascension/GameCore/Modules/TeamManager/TeamManager.cs
+++ GameServer/AscensionServer/Ascension/GameCore/Modules/TeamManager/TeamManager.cs
@@ -1,114 +1,114 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Concurrent;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
-using Cosmos;
-namespace AscensionServer
-{
-    public class TeamManager : ModuleBase<TeamManager>
-    {
-        /// <summary>
-        /// 房间ID长度
-        /// </summary>
-        readonly int _IDLenth = 6;
-        /// <summary>
-        /// 生成房间时最小取值范围
-        /// </summary>
-        readonly int _MinValue = 1000000;
-        /// <summary>
-        /// 成功房间时最大取值范围
-        /// </summary>
-        readonly int _MaxValue = 99999999;
-        CancellationToken cancelToken = new CancellationToken();
-        ConcurrentDictionary<int, TeamCache> teamDict = new ConcurrentDictionary<int, TeamCache>();
-        ConcurrentVariable<HashSet<int>> matchingQueue = new ConcurrentVariable<HashSet<int>>();
-        public TeamCache CreateTeam(int createrID)
-        {
-            if (teamDict.ContainsKey(createrID))
-                return null;
-            var tc = ConcurrentSingleton<ReferencePoolManager>.Instance.Spawn<TeamCache>();
-            tc.InitTeam(createrID,CreateTeamID());
-            return tc;
-        }
-        public bool LeaveMatchQueue(int peerID)
-        {
-            return matchingQueue.Data.Remove(peerID);
-        }
-        /// <summary>
-        ///加入随机匹配队列
-        /// </summary>
-        public bool JoinMatchQueue(int peerID)
-        {
-            return matchingQueue.Data.Add(peerID);
-        }
-        /// <summary>
-        /// 服务端会一直运行；
-        /// 异步随机匹配组队
-        /// </summary>
-        /// <returns>异步运行</returns>
-        public async Task MatchTeam()
-        {
-            while (true)
-            {
-                if (!cancelToken.IsCancellationRequested)
-                {
-                    if (teamDict.Count <= 0 || matchingQueue.Data.Count <= 0)
-                        return;
-                    HashSet<int> removeIDSet = new HashSet<int>();
-                    int teamID = -1;
-                    foreach (var team in teamDict.Values)
-                    {
-                        if (!team.IsFull)
-                            teamID=team.TeamID;
-                    }
-                    await Task.Run(() =>
-                    {
-                        foreach (var id in matchingQueue.Data)
-                        {
-                            if (teamID == -1)
-                                continue;
-                            TeamCache tc;
-                            teamDict.TryGetValue(teamID, out tc);
-                            tc.JoinTeam(id);
-                            removeIDSet.Add(id);
-                        }
-                        foreach (var id in removeIDSet)
-                        {
-                            matchingQueue.Data.Remove(id);
-                        }
-                    });
-                }
-            }
-        }
-        /// <summary>
-        /// 加入指定的小队
-        /// </summary>
-        /// <param name="teamID">小队ID</param>
-        /// <param name="peerID">peerID</param>
-        /// <returns>是否加入成功</returns>
-        public bool JoinTeam(int teamID, int peerID)
-        {
-            TeamCache gc;
-            if (!teamDict.TryGetValue(teamID, out gc))
-                return false;
-            else
-                return gc.JoinTeam(peerID);
-        }
-        /// <summary>
-        /// 生成队伍ID；
-        /// 尾递归检测是否生成了同样的key
-        /// </summary>
-        /// <returns>生成后的ID</returns>
-        int CreateTeamID()
-        {
-            int id=Utility.Algorithm.CreateRandomInt( _MinValue, _MaxValue);
-            if (!teamDict.ContainsKey(id))
-                return id;
-            else
-                return CreateTeamID();
-        }
-    }
-}
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Collections.Concurrent;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+//using System.Threading;
+//using Cosmos;
+//namespace AscensionServer
+//{
+//    public class TeamManager : ModuleBase<TeamManager>
+//    {
+//        /// <summary>
+//        /// 房间ID长度
+//        /// </summary>
+//        readonly int _IDLenth = 6;
+//        /// <summary>
+//        /// 生成房间时最小取值范围
+//        /// </summary>
+//        readonly int _MinValue = 1000000;
+//        /// <summary>
+//        /// 成功房间时最大取值范围
+//        /// </summary>
+//        readonly int _MaxValue = 99999999;
+//        CancellationToken cancelToken = new CancellationToken();
+//        ConcurrentDictionary<int, TeamCache> teamDict = new ConcurrentDictionary<int, TeamCache>();
+//        //ConcurrentVariable<HashSet<int>> matchingQueue = new ConcurrentVariable<HashSet<int>>();
+//        public TeamCache CreateTeam(int createrID)
+//        {
+//            if (teamDict.ContainsKey(createrID))
+//                return null;
+//            var tc = ConcurrentSingleton<ReferencePoolManager>.Instance.Spawn<TeamCache>();
+//            tc.InitTeam(createrID,CreateTeamID());
+//            return tc;
+//        }
+//        public bool LeaveMatchQueue(int peerID)
+//        {
+//            return matchingQueue.Data.Remove(peerID);
+//        }
+//        /// <summary>
+//        ///加入随机匹配队列
+//        /// </summary>
+//        public bool JoinMatchQueue(int peerID)
+//        {
+//            return matchingQueue.Data.Add(peerID);
+//        }
+//        /// <summary>
+//        /// 服务端会一直运行；
+//        /// 异步随机匹配组队
+//        /// </summary>
+//        /// <returns>异步运行</returns>
+//        public async Task MatchTeam()
+//        {
+//            while (true)
+//            {
+//                if (!cancelToken.IsCancellationRequested)
+//                {
+//                    if (teamDict.Count <= 0 || matchingQueue.Data.Count <= 0)
+//                        return;
+//                    HashSet<int> removeIDSet = new HashSet<int>();
+//                    int teamID = -1;
+//                    foreach (var team in teamDict.Values)
+//                    {
+//                        if (!team.IsFull)
+//                            teamID=team.TeamID;
+//                    }
+//                    await Task.Run(() =>
+//                    {
+//                        foreach (var id in matchingQueue.Data)
+//                        {
+//                            if (teamID == -1)
+//                                continue;
+//                            TeamCache tc;
+//                            teamDict.TryGetValue(teamID, out tc);
+//                            tc.JoinTeam(id);
+//                            removeIDSet.Add(id);
+//                        }
+//                        foreach (var id in removeIDSet)
+//                        {
+//                            matchingQueue.Data.Remove(id);
+//                        }
+//                    });
+//                }
+//            }
+//        }
+//        /// <summary>
+//        /// 加入指定的小队
+//        /// </summary>
+//        /// <param name="teamID">小队ID</param>
+//        /// <param name="peerID">peerID</param>
+//        /// <returns>是否加入成功</returns>
+//        public bool JoinTeam(int teamID, int peerID)
+//        {
+//            TeamCache gc;
+//            if (!teamDict.TryGetValue(teamID, out gc))
+//                return false;
+//            else
+//                return gc.JoinTeam(peerID);
+//        }
+//        /// <summary>
+//        /// 生成队伍ID；
+//        /// 尾递归检测是否生成了同样的key
+//        /// </summary>
+//        /// <returns>生成后的ID</returns>
+//        int CreateTeamID()
+//        {
+//            int id=Utility.Algorithm.CreateRandomInt( _MinValue, _MaxValue);
+//            if (!teamDict.ContainsKey(id))
+//                return id;
+//            else
+//                return CreateTeamID();
+//        }
+//    }
+//}
