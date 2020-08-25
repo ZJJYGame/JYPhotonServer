063e4f01eb273c689ff1160ce942118aed43d7b0
1150463081 <63703895+1150463081@users.noreply.github.com>
2020-08-25 20:53:39 +0800

    ljyedit

--- GameServer/AscensionServer/Handlers/SyncAuction/SubHandler/GetAuctionSubHandler.cs
+++ GameServer/AscensionServer/Handlers/SyncAuction/SubHandler/GetAuctionSubHandler.cs
@@ -24,11 +24,18 @@ namespace AscensionServer
         public override void Handler(OperationRequest operationRequest, SendParameters sendParameters, AscensionPeer peer)
         {
             var dict = ParseSubDict(operationRequest);
-            int auctionGoodsID = Convert.ToInt32(Utility.GetValue(dict, (byte)ParameterCode.Auction));
-            AscensionServer._Log.Info("收到的拍卖行"+ auctionGoodsID);
+            string dictJson = Convert.ToString(Utility.GetValue(dict, (byte)ParameterCode.Auction));
+            Dictionary<string, int> tempDict = Utility.Json.ToObject<Dictionary<string, int>>(dictJson);
+            int auctionGoodsID = tempDict["ID"];
+            int startIndex = tempDict["Start"];
+            int endIndex = tempDict["End"];
+            int goodsCount=0;
+
+            AscensionServer._Log.Info("收到的拍卖行"+ auctionGoodsID+"起始"+startIndex+"结束"+endIndex);
 
             List<AuctionGoodsIndex> result=null;
             List<AuctionGoodsDTO> auctionGoodsDTOList = new List<AuctionGoodsDTO>();
+            List<AuctionGoodsDTO> resultAuctionGoodsDTOList = new List<AuctionGoodsDTO>();
             var isHasValue = RedisHelper.Hash.HashExistAsync("AuctionIndex", auctionGoodsID.ToString()).Result;
             if (!isHasValue)
             {
@@ -38,16 +45,22 @@ namespace AscensionServer
             {
                 AscensionServer._Log.Info("redis拍卖行索引表存在该ID");
                 result = RedisHelper.Hash.HashGet<List<AuctionGoodsIndex>>("AuctionIndex", auctionGoodsID.ToString());
-               
+                goodsCount = result.Count;
                 for (int i = 0; i < result.Count; i++)
                 {
-                    AscensionServer._Log.Info("1");
                     string auctionGoodsJson = RedisHelper.String.StringGetAsync("AuctionGoods"+result[i].RedisKey).Result;
-                    AscensionServer._Log.Info(auctionGoodsJson);
                     auctionGoodsDTOList.Add(Utility.Json.ToObject<AuctionGoodsDTO>(auctionGoodsJson));
-                    AscensionServer._Log.Info("3");
                 }
-                
+
+                if (endIndex + 1 <= goodsCount)
+                {
+                    resultAuctionGoodsDTOList = auctionGoodsDTOList.GetRange(startIndex, endIndex);
+                }
+                else
+                {
+                    resultAuctionGoodsDTOList = auctionGoodsDTOList.GetRange(startIndex, goodsCount - 1);
+                }
+                //todo发送数据更改
             }
             SetResponseData(() =>
             {
