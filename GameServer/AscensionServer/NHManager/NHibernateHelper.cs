797c2b6d5cc2d0602ad4d498e4f6a0a975336eae
英短 <49228668+Zt01221510@users.noreply.github.com>
2020-08-31 09:23:32 +0800

    edit

--- GameServer/AscensionServer/NHManager/NHibernateHelper.cs
+++ GameServer/AscensionServer/NHManager/NHibernateHelper.cs
@@ -29,10 +29,10 @@ namespace AscensionServer
 
                     _sessionFactory = Fluently.Configure().
                         Database(MySQLConfiguration.Standard.
-                        //ConnectionString(db => db.Server("192.168.0.117").
+                        ConnectionString(db => db.Server("192.168.0.117").
                         //ConnectionString(db => db.Server("60.12.176.54").
-                        ConnectionString(db => db.Server("127.0.0.1").
-                        Database("jygame").Username("root").
+                        //ConnectionString(db => db.Server("127.0.0.1").
+                        Database("jygame").Username("jieyou").
                         Password("jieyougamePWD"))).
                         Mappings(x => { x.FluentMappings.AddFromAssemblyOf<NHibernateHelper>(); }).
                         BuildSessionFactory(); 
