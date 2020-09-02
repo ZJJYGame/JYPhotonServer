1fbac7932858095d681bd1d1c6c8aae48f654fa1
英短 <49228668+Zt01221510@users.noreply.github.com>
2020-09-02 15:03:34 +0800

    edit:注释不知道是否有用的代码

--- GameServer/AscensionServer/AlliancelogicManager/AllianceEvent.cs
+++ GameServer/AscensionServer/AlliancelogicManager/AllianceEvent.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AscensionProtocol.DTO;
+using AscensionServer.Model;
 using Cosmos;
 namespace AscensionServer
 {
-   public class AllianceEvent: ConcurrentEventCore<string,List<int> , AllianceEvent>
+   public class AllianceEvent: ConcurrentEventCore<int, AscensionPeer, AllianceEvent>
     {
         /// <summary>
         /// 监听仙盟解散的事件
         /// </summary>
         /// <param name="key"></param>
         /// <param name="handler"></param>
-        public override void AddEventListener(string key, Action<List<int>> handler)
+        public override void AddEventListener(int key, Action<AscensionPeer> handler)
         {
             base.AddEventListener(key, handler);
         }
@@ -22,18 +24,36 @@ namespace AscensionServer
         /// </summary>
         /// <param name="key"></param>
         /// <param name="handler"></param>
-        public override void RemoveEventListener(string key, Action<List<int>> handler)
+        public override void RemoveEventListener(int key, Action<AscensionPeer> handler)
         {
             base.RemoveEventListener(key, handler);
         }
         /// <summary>
         /// 清除自身仙盟数据函数
         /// </summary>
-        /// <param name="id">下标0为自身id，下标1为仙盟id</param>
-        public void RemoveAllianceStatus(List<int> id)
+        /// <param name="id">下标0为自身id</param>
+        public void RemoveAllianceStatus(int id)
         {
-            Utility.Debug.LogError("监听解散的请求成功");
-        }
 
+            //var roleObj = AlliancelogicManager.instance.GetNHCriteria<RoleAlliance>("RoleID", peer.PeerCache.RoleID);
+            //roleObj.AllianceID = 0;
+            //ConcurrentSingleton<NHManager>.Instance.UpdateAsync(roleObj);
+
+            //Utility.Debug.LogError("监听解散的请求成功,这个人的id为"+peer.PeerCache.RoleID);
+            //var applyList = Utility.Json.ToObject<List<int>>(roleObj.ApplyForAlliance);
+
+        }
+        //public void RemoveAllianceStatus(List<int> id)
+        //{
+
+        //    var roleObj = AlliancelogicManager.instance.GetNHCriteria<RoleAlliance>("RoleID", id[0]);
+        //    var applyList = Utility.Json.ToObject<List<int>>(roleObj.ApplyForAlliance);
+        //    applyList.Remove(id[1]);
+        //    roleObj.ApplyForAlliance = Utility.Json.ToJson(applyList);
+        //    ConcurrentSingleton<NHManager>.Instance.UpdateAsync(roleObj);
+
+        //    Utility.Debug.LogError("监听解散的请求成功,这个人的id为", id[0]);
+
+        //}
     }
 }
