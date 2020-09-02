44f3d36915a257445837f23796373d0ae54821f8
1150463081 <63703895+1150463081@users.noreply.github.com>
2020-09-02 10:45:48 +0800

    ljyedit

---
+++ GameServer/AscensionServer/Handlers/SyncRoleAuctionItems/SyncRoleAuctionItemsHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AscensionProtocol;
+
+namespace AscensionServer
+{
+    public class SyncRoleAuctionItemsHandler:Handler
+    {
+        public override void OnInitialization()
+        {
+            OpCode = OperationCode.SyncRoleAuctionItems;
+            base.OnInitialization();
+            OnSubHandlerInitialization<SyncRoleAuctionItemsSubHandler>();
+        }
+    }
+}
