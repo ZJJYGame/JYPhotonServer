220c2df393a466ebc3ba9999160d53fd2e4b187c
1150463081 <63703895+1150463081@users.noreply.github.com>
2020-09-03 10:27:11 +0800

    Edit:更新了新的拍卖行数据库结构，个人上架信息

--- GameServer/AscensionProtocol/DTO/RoleAuctionItemsDTO.cs
+++ GameServer/AscensionProtocol/DTO/RoleAuctionItemsDTO.cs
@@ -10,11 +10,20 @@ namespace AscensionProtocol.DTO
     public class RoleAuctionItemsDTO : DataTransferObject
     {
         public int RoleID { get; set; }
-        public List<int> AuctionItemsGuidList { get; set; }
+        public List<RoleAuctionItem> AuctionItemsGuidList { get; set; }
         public override void Clear()
         {
             RoleID = 0;
             AuctionItemsGuidList = null;
         }
     }
+    [Serializable]
+    public class RoleAuctionItem
+    {
+        public AuctionGoodsDTO AuctionGoods { get; set; }
+        public bool IsPutAway { get; set; }
+    }
 }
+
+
+  
--- GameServer/AscensionServer/Handlers/SyncRoleAuctionItems/SubHandler/GetRoleAuctionItemsSubHandler.cs
+++ GameServer/AscensionServer/Handlers/SyncRoleAuctionItems/SubHandler/GetRoleAuctionItemsSubHandler.cs
@@ -23,7 +23,39 @@ namespace AscensionServer
 
         public override void Handler(OperationRequest operationRequest, SendParameters sendParameters, AscensionPeer peer)
         {
-            Utility.Debug.LogInfo("收到获得个人上架物品请求");
+            var dict = ParseSubDict(operationRequest);
+            int roleID = Convert.ToInt32(Utility.GetValue(dict, (byte)ParameterCode.RoleAuctionItems));
+            List<RoleAuctionItem> roleAuctionItemList = new List<RoleAuctionItem>();
+            if (RedisHelper.Hash.HashExistAsync("RoleAuctionItems", roleID.ToString()).Result)
+            {
+                List<string> tempGuidList = RedisHelper.Hash.HashGetAsync<List<string>>("RoleAuctionItems", roleID.ToString()).Result;
+                for (int i = 0; i < tempGuidList.Count; i++)
+                {
+                    if (RedisHelper.String.StringGetAsync("AuctionGoods_" + tempGuidList[i]).Result != null)
+                    {
+                        roleAuctionItemList.Add(new RoleAuctionItem()
+                        {
+                            AuctionGoods=RedisHelper.Hash.HashGetAsync<AuctionGoodsDTO>("AuctionGoodsData", "AuctionGoods_" + tempGuidList[i]).Result,
+                            IsPutAway=true
+                        });
+                    }
+                    else
+                    {
+                        roleAuctionItemList.Add(new RoleAuctionItem()
+                        {
+                            AuctionGoods = RedisHelper.Hash.HashGetAsync<AuctionGoodsDTO>("AuctionGoodsData", "AuctionGoods_" + tempGuidList[i]).Result,
+                            IsPutAway = false
+                        });
+                    }
+                }
+            }
+            SetResponseData(() =>
+            {
+                Utility.Debug.LogInfo(Utility.Json.ToJson(roleAuctionItemList));
+                SubDict.Add((byte)ParameterCode.RoleAuctionItems, Utility.Json.ToJson(roleAuctionItemList));
+                Owner.OpResponse.ReturnCode = (short)ReturnCode.Success;
+            });
+            peer.SendOperationResponse(Owner.OpResponse, sendParameters);
         }
     }
 }
