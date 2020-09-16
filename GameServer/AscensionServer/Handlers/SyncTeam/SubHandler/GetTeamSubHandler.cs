34c4bb73799144e346ccccfc58cd6a3b87e9c11b
Zt01221510 <39549944+nqclymt@users.noreply.github.com>
2020-09-16 14:41:06 +0800

    Update 队伍指令
    Update 队伍指令

--- GameServer/AscensionServer/Handlers/SyncTeam/SubHandler/GetTeamSubHandler.cs
+++ GameServer/AscensionServer/Handlers/SyncTeam/SubHandler/GetTeamSubHandler.cs
@@ -12,10 +12,10 @@ using Renci.SshNet.Security;
 using Cosmos;
 namespace AscensionServer
 {
-    public class GetTeamSubHandler:SyncTeamSubHandler
+    public class GetTeamSubHandler : SyncTeamSubHandler
     {
         public override void OnInitialization() => SubOpCode = SubOperationCode.Get;
-       
+
         public override void Handler(OperationRequest operationRequest, SendParameters sendParameters, AscensionPeer peer)
         {
             ResetResponseData(operationRequest);
@@ -24,17 +24,26 @@ namespace AscensionServer
             var RoleObj = Utility.Json.ToObject<RoleDTO>(teamData);
             NHCriteria nHCriteriaRoleID = GameManager.ReferencePoolManager.Spawn<NHCriteria>().SetValue("RoleID", RoleObj.RoleID);
             bool exist = ConcurrentSingleton<NHManager>.Instance.Verify<RoleRing>(nHCriteriaRoleID);
-            if (!exist)
-                Owner.OpResponse.ReturnCode = (short)ReturnCode.Empty;
-            else
+
+            if (exist)
             {
-                if (!AscensionServer.Instance._playerIdToTeamIdDict.ContainsKey(RoleObj.RoleID))
+                switch (RoleObj.teamInstructions)
                 {
-                    AscensionServer.Instance.CreateTeam(RoleObj, new int[] { 0 ,99 });
-                    Owner.ResponseData.Add((byte)ParameterCode.RoleTeam, AscensionServer.Instance._teamTOModel);
-                    Owner.OpResponse.ReturnCode = (short)ReturnCode.Success;
+                    case RoleDTO.TeamInstructions.CreateTeam:
+                        if (!AscensionServer.Instance._playerIdToTeamIdDict.ContainsKey(RoleObj.RoleID))
+                            AscensionServer.Instance.CreateTeam(RoleObj, new int[] { 0, 99 });
+                        break;
+                    default:
+                        break;
                 }
+                Owner.ResponseData.Add((byte)ParameterCode.RoleTeam, Utility.Json.ToJson(AscensionServer.Instance._teamTOModel));
+                Owner.ResponseData.Add((byte)ParameterCode.Role, Utility.Json.ToJson(AscensionServer.Instance._playerIdToTeamIdDict));
+                Owner.OpResponse.Parameters = Owner.ResponseData;
+                Owner.OpResponse.ReturnCode = (short)ReturnCode.Success;
             }
+            else
+                Owner.OpResponse.ReturnCode = (short)ReturnCode.Fail;
+
             peer.SendOperationResponse(Owner.OpResponse, sendParameters);
             GameManager.ReferencePoolManager.Despawn(nHCriteriaRoleID);
         }
