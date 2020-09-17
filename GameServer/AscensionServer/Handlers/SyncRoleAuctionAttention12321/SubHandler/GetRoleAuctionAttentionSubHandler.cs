4557a2bfddfe90b17c8a3b61656b42c4c451beb8
1150463081 <63703895+1150463081@users.noreply.github.com>
2020-09-17 19:07:22 +0800

    Update:恢复丢失的文件

---
+++ GameServer/AscensionServer/Handlers/SyncRoleAuctionAttention12321/SubHandler/GetRoleAuctionAttentionSubHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AscensionProtocol;
+using AscensionProtocol.DTO;
+using Photon.SocketServer;
+using AscensionServer.Model;
+using Cosmos;
+using RedisDotNet;
+using StackExchange.Redis;
+
+namespace AscensionServer
+{
+    public class GetRoleAuctionAttentionSubHandler : SyncRoleAuctionAttentionSubHandler
+    {
+        public override void OnInitialization()
+        {
+            SubOpCode = SubOperationCode.Get;
+            base.OnInitialization();
+        }
+
+        public async override void Handler(OperationRequest operationRequest, SendParameters sendParameters, AscensionPeer peer)
+        {
+            ResetResponseData(operationRequest);
+            var dict = ParseSubDict(operationRequest);
+            int roleID= Convert.ToInt32(Utility.GetValue(dict, (byte)ParameterCode.RoleAuctionItems));
+
+            List<AuctionGoodsDTO> resultAuctionGoodsList = new List<AuctionGoodsDTO>();
+
+            if(RedisHelper.Hash.HashExistAsync("RoleAuctionAttention", roleID.ToString()).Result)
+            {
+                List<string> guidList= RedisHelper.Hash.HashGetAsync<List<string>>("RoleAuctionAttention", roleID.ToString()).Result;
+                for (int i = guidList.Count-1; i >=0; i--)
+                {
+                    if (RedisHelper.String.StringGetAsync("AuctionGoods_" + guidList[i]).Result == null)
+                    {
+                        guidList.RemoveAt(i);
+                    }
+                }
+                await RedisHelper.Hash.HashSetAsync("RoleAuctionAttention", roleID.ToString(), guidList);
+                for (int i = 0; i < guidList.Count; i++)
+                {
+                    AuctionGoodsDTO tempAuctionGoodsDTO = RedisHelper.Hash.HashGetAsync<AuctionGoodsDTO>("AuctionGoodsData",guidList[i]).Result;
+                    resultAuctionGoodsList.Add(tempAuctionGoodsDTO);
+                }
+            }
+            Owner.ResponseData.Add((byte)ParameterCode.RoleAuctionItems, Utility.Json.ToJson(resultAuctionGoodsList));
+            Owner.OpResponse.Parameters = Owner.ResponseData;
+            Owner.OpResponse.ReturnCode = (short)ReturnCode.Success;
+            peer.SendOperationResponse(Owner.OpResponse, sendParameters);
+        }
+    }
+}
---
+++ GameServer/AscensionServer/Handlers/SyncRoleAuctionAttention12321/SyncRoleAuctionAttentionHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AscensionProtocol;
+
+namespace AscensionServer
+{
+    public class SyncRoleAuctionAttentionHandler:Handler
+    {
+        public override void OnInitialization()
+        {
+            OpCode = OperationCode.SyncRoleAuctionAttention;
+            base.OnInitialization();
+            OnSubHandlerInitialization<SyncRoleAuctionAttentionSubHandler>();
+        }
+    }
+}
