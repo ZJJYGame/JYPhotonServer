fe953b7a637a121549157ce3e891b83a7fdaa55f
Zt01221510 <202028014@qq.com>
2020-09-27 15:09:29 +0800

    Update 定义 进入战斗传输数据类型
    Update 定义 进入战斗传输数据类型

---
+++ GameServer/AscensionServer/Ascension/Core/RunTime/Battle/ServerBattleManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Cosmos;
+namespace AscensionServer
+{
+    /// <summary>
+    /// 没统一的服务器战斗功能
+    /// </summary>
+    [CustomeModule]
+    public class ServerBattleManager:Module<ServerBattleManager>
+    {
+        //public Dictionary<int,int> 
+        /*
+         * 
+         * 3.
+         * 2.加载resource json数据表
+         * 1.需要怎么存，怎么管理*/
+
+    }
+}
