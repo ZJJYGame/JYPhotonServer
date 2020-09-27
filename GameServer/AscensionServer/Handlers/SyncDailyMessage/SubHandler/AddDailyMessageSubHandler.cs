0e92608033d197e48281288c15e76360b5780be6
英短 <49228668+Zt01221510@users.noreply.github.com>
2020-09-27 14:11:58 +0800

    Update:新加的仙盟日常消息内容的获取增加

---
+++ GameServer/AscensionServer/Handlers/SyncDailyMessage/SubHandler/AddDailyMessageSubHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AscensionProtocol;
+using Photon.SocketServer;
+using AscensionServer.Model;
+using Cosmos;
+using AscensionProtocol.DTO;
+using RedisDotNet;
+
+namespace AscensionServer
+{
+  public class AddDailyMessageSubHandler: SyncDailyMessageSubHandler
+    {
+        public override byte SubOpCode { get; protected set; } = (byte)SubOperationCode.Add;
+
+
+    }
+}
