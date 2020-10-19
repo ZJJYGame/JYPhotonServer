a52a43d9c3d41101c0974541bfdf2c9671a9f301
英短 <49228668+Zt01221510@users.noreply.github.com>
2020-10-19 15:17:56 +0800

    update:新加瓶颈心魔json数据及初始化

---
+++ GameServer/AscensionServer/Ascension/Core/Base/DataObject/BottleneckData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AscensionServer
+{
+    //瓶颈突破数据表
+    /// <summary>
+    /// 角色等级 
+    /// 灵根数
+    /// 天雷类型
+    /// 雷劫回合数
+    /// </summary>
+    [Serializable]
+    [ConfigData]
+   public class BottleneckData
+    {
+        public int Index { get; set; }
+        public int Level_ID { get; set; }
+        public List<int> Spiritual_Root_1 { get; set; }
+        public List<int> Spiritual_Root_2 { get; set; }
+        public List<int> Spiritual_Root_3 { get; set; }
+        public List<int> Spiritual_Root_4 { get; set; }
+        public List<int> Spiritual_Root_5 { get; set; }
+        public int Thunder_ID { get; set; }
+        public int Thunder_Round { get; set; }
+    }
+}
