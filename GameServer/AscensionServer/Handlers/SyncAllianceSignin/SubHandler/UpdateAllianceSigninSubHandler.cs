1ebeaee0f632d6dd7c8ea7cd448017138ee60523
英短 <49228668+Zt01221510@users.noreply.github.com>
2020-10-23 15:01:00 +0800

    update:新增仙盟签到奖励数据跟每日自动扣除人气灵石数据

--- GameServer/AscensionServer/Handlers/SyncAllianceSignin/SubHandler/UpdateAllianceSigninSubHandler.cs
+++ GameServer/AscensionServer/Handlers/SyncAllianceSignin/SubHandler/UpdateAllianceSigninSubHandler.cs
@@ -10,7 +10,6 @@ using AscensionServer.Model;
 using Cosmos;
 using RedisDotNet;
 using StackExchange.Redis;
-
 namespace AscensionServer
 {
     public class UpdateAllianceSigninSubHandler : SyncAllianceSigninSubHandler
@@ -27,30 +26,58 @@ namespace AscensionServer
             var allianceTemp = AlliancelogicManager.Instance.GetNHCriteria<AllianceStatus>("ID", allianceSigninObj.AllianceID);
 
             var allianceConstructionTemp = AlliancelogicManager.Instance.GetNHCriteria<AllianceConstruction>("AllianceID", allianceSigninObj.AllianceID);
-            List<string> signinList = new List<string>();
-            if (roleallianceTemp != null && allianceTemp != null && allianceConstructionTemp != null)
+            var Role = AlliancelogicManager.Instance.GetNHCriteria<Role>("RoleID", roleallianceTemp.RoleID);
+
+            GameManager.CustomeModule<DataManager>().TryGetValue<List<AllianceSigninData>>(out var allianceSigninList);
+            var content = RedisHelper.KeyExistsAsync("AllianceSigninDTO"+ allianceTemp.ID).Result;
+
+
+          var alliancesignindata=  allianceSigninList.Find(t=>t.Role_Level[0]< Role.RoleLevel&& t.Role_Level[1] < Role.RoleLevel);
+            if (!content)
             {
-                roleallianceTemp.Reputation += allianceSigninObj.RoleContribution;
-                roleallianceTemp.ReputationHistroy += allianceSigninObj.RoleContribution;
-                roleallianceTemp.ReputationMonth += allianceSigninObj.RoleContribution;
-
-                allianceTemp.Popularity += allianceSigninObj.Popularity;
-                allianceConstructionTemp.AllianceAssets += allianceSigninObj.AllianceSpiritStone;
-                var Role = AlliancelogicManager.Instance.GetNHCriteria<Role>("RoleID", roleallianceTemp.RoleID);
-                RoleAllianceDTO roleAllianceDTO = new RoleAllianceDTO() { AllianceID = roleallianceTemp.AllianceID, AllianceJob = roleallianceTemp.AllianceJob, JoinTime = roleallianceTemp.JoinTime, ApplyForAlliance = Utility.Json.ToObject<List<int>>(roleallianceTemp.ApplyForAlliance), JoinOffline = roleallianceTemp.JoinOffline, Reputation = roleallianceTemp.Reputation, ReputationHistroy = roleallianceTemp.ReputationHistroy, ReputationMonth = roleallianceTemp.ReputationMonth, RoleID = roleallianceTemp.RoleID, RoleName = roleallianceTemp.RoleName, RoleSchool = roleallianceTemp.RoleSchool, RoleLevel = Role.RoleLevel };
-
-                signinList.Add(Utility.Json.ToJson(roleAllianceDTO));
-                signinList.Add(Utility.Json.ToJson(allianceTemp));
-                signinList.Add(Utility.Json.ToJson(allianceConstructionTemp));
-                NHibernateQuerier.Update(roleallianceTemp);
-                NHibernateQuerier.Update(allianceTemp);
-                NHibernateQuerier.Update(allianceConstructionTemp);
-                SetResponseParamters(() =>
+                if (alliancesignindata != null)
                 {
-                    Utility.Debug.LogError("发送回去的兑换弹药的请求数据" + Utility.Json.ToJson(signinList));
-                    subResponseParameters.Add((byte)ParameterCode.AllianceSignin, Utility.Json.ToJson(signinList));
-                    operationResponse.ReturnCode = (short)ReturnCode.Success;
-                });
+                    List<string> signinList = new List<string>();
+                    if (roleallianceTemp != null && allianceTemp != null && allianceConstructionTemp != null)
+                    {
+                        roleallianceTemp.Reputation += alliancesignindata.Role_Contribution;
+                        roleallianceTemp.ReputationHistroy += alliancesignindata.Role_Contribution;
+                        roleallianceTemp.ReputationMonth += alliancesignindata.Role_Contribution;
+
+                        allianceTemp.Popularity += alliancesignindata.Alliance_Popularity;
+                        allianceConstructionTemp.AllianceAssets += alliancesignindata.Alliance_Spirit_Stone;
+
+                        RoleAllianceDTO roleAllianceDTO = new RoleAllianceDTO() { AllianceID = roleallianceTemp.AllianceID, AllianceJob = roleallianceTemp.AllianceJob, JoinTime = roleallianceTemp.JoinTime, ApplyForAlliance = Utility.Json.ToObject<List<int>>(roleallianceTemp.ApplyForAlliance), JoinOffline = roleallianceTemp.JoinOffline, Reputation = roleallianceTemp.Reputation, ReputationHistroy = roleallianceTemp.ReputationHistroy, ReputationMonth = roleallianceTemp.ReputationMonth, RoleID = roleallianceTemp.RoleID, RoleName = roleallianceTemp.RoleName, RoleSchool = roleallianceTemp.RoleSchool, RoleLevel = Role.RoleLevel };
+
+                        signinList.Add(Utility.Json.ToJson(roleAllianceDTO));
+                        signinList.Add(Utility.Json.ToJson(allianceTemp));
+                        signinList.Add(Utility.Json.ToJson(allianceConstructionTemp));
+                        NHibernateQuerier.Update(roleallianceTemp);
+                        NHibernateQuerier.Update(allianceTemp);
+                        NHibernateQuerier.Update(allianceConstructionTemp);
+                        RedisHelper.String.StringGetSet("AllianceSigninDTO"+ allianceTemp.ID, "AllianceSigninDTO" + allianceTemp.ID);
+                        SetResponseParamters(() =>
+                        {
+                            Utility.Debug.LogError("发送回去的兑换弹药的请求数据" + Utility.Json.ToJson(signinList));
+                            subResponseParameters.Add((byte)ParameterCode.AllianceSignin, Utility.Json.ToJson(signinList));
+                            operationResponse.ReturnCode = (short)ReturnCode.Success;
+                        });
+                    }
+                    else
+                    {
+                        SetResponseParamters(() =>
+                        {
+                            operationResponse.ReturnCode = (short)ReturnCode.Fail;
+                        });
+                    }
+                }
+                else
+                {
+                    SetResponseParamters(() =>
+                    {
+                        operationResponse.ReturnCode = (short)ReturnCode.Fail;
+                    });
+                }
             }
             else
             {
@@ -59,7 +86,6 @@ namespace AscensionServer
                     operationResponse.ReturnCode = (short)ReturnCode.Fail;
                 });
             }
-            //peer.SendOperationResponse(Owner.OpResponseData, sendParameters);
             return operationResponse;
         }
     }
