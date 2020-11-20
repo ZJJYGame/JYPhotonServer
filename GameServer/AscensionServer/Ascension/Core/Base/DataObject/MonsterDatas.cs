223b6afad008dd5a7a8d4307e258b6b37992031a
Zt01221510 <202028014@qq.com>
2020-11-20 14:44:07 +0800

    Update ai更改
    Update ai更改

--- GameServer/AscensionServer/Ascension/Core/Base/DataObject/MonsterDatas.cs
+++ GameServer/AscensionServer/Ascension/Core/Base/DataObject/MonsterDatas.cs
@@ -11,34 +11,31 @@ namespace AscensionServer
     [ConfigData]
     public class MonsterDatas
     {
-        public int ID { get; set; }
         public int Monster_ID { get; set; }
-        public string Monster_name { get; set; }
-        public string Monster_describe { get; set; }
-        public string Monster_level { get; set; }
-        public int Role_HP { get; set; }
-        public int Role_MP { get; set; }
-        public int Role_soul { get; set; }
-        public float Attact_speed { get; set; }
-        public int Attact_physical { get; set; }
-        public int Defence_physical { get; set; }
-        public int Attact_power { get; set; }
-        public int Defence_power { get; set; }
-        public int Attact_soul { get; set; }
-        public int Defence_soul { get; set; }
-        public int UP_double { get; set; }
-        public int Down_double { get; set; }
-        public int Value_flow { get; set; }
+        public string Monster_Name { get; set; }
+        public string Monster_Describe { get; set; }
+        public string Monster_Level { get; set; }
+        public int Monster_Gig_Level { get; set; }
+        public int Monster_HP { get; set; }
+        public int Monster_MP { get; set; }
+        public int Monster_Soul { get; set; }
+        public float Attact_Speed { get; set; }
+        public int Attact_Physical { get; set; }
+        public int Defend_Physical { get; set; }
+        public int Attact_Power { get; set; }
+        public int Defend_Power { get; set; }
+        public int Value_Flow { get; set; }
         public  int PhysicalCritProb { get; set; }
         public  int MagicCritProb { get; set; }
         public  int ReduceCritProb { get; set; }
         public  int PhysicalCritDamage { set; get; }
         public  int MagicCritDamage { get; set; }
         public  int ReduceCritDamage { get; set; }
-        public int Alert_area { get; set; }
-        public int Move_speed { get; set; }
-        public int Value_hide { get; set; }
-        public int Best_blood { get; set; }
+        public int Alert_Area { get; set; }
+        public int Move_Speed { get; set; }
+        public bool IsHide { get; set; }
+        public int Value_Hide { get; set; }
+        public int Best_Blood { get; set; }
         public List<int> Skill_Array { get; set; }
         public List<int> Drop_Array { get; set; }
         public List<int> Drop_Rate { get; set; }
