7867e671e5f86966d9cd9dc09ccb7e5903f37e3d
DonHitYep <zhengdonn@gmail.com>
2021-01-21 14:11:54 +0800

    Update：提交服务器修改；

--- GameServer/AscensionServer/Ascension/Core/Base/NHibernate/NHibernateHelper.cs
+++ GameServer/AscensionServer/Ascension/Core/Base/NHibernate/NHibernateHelper.cs
@@ -25,8 +25,8 @@ namespace AscensionServer
                     _sessionFactory = Fluently.Configure().
                         Database(MySQLConfiguration.Standard.
 
-                        //ConnectionString(db => db.Server("127.0.0.1").Database("jygame").Username("root").Password("jieyougamePWD"))).//公网
-                        ConnectionString(db => db.Server("192.168.0.117").Database("jygame").Username("jieyou").Password("jieyougamePWD"))).//内网
+                        ConnectionString(db => db.Server("127.0.0.1").Database("jygame").Username("jieyou").Password("jieyougamePWD"))).//公网
+                                                                                                                                      //ConnectionString(db => db.Server("192.168.0.117").Database("jygame").Username("jieyou").Password("jieyougamePWD"))).//内网
                         Mappings(x => { x.FluentMappings.AddFromAssemblyOf<NHibernateHelper>(); }).
                         BuildSessionFactory();
                 }
--- GameServer/Runner/RoleStatusDatas.cs
+++ GameServer/Runner/RoleStatusDatas.cs
@@ -4,34 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Runner
-{
-    [Serializable]
-   public class RoleStatusDatas
-    {
-        public int LevelID { get; set; }
-        public int NextLevelID { get; set; }
-        public int FreeAttributes { get; set; }
-        public int RoleHP { get; set; }
-        public int RoleMP { get; set; }
-        public int RoleSoul { get; set; }
-        public int BestBlood { get; set; }
-        public int AttackSpeed { get; set; }
-        public int AttackPhysical { get; set; }
-        public int DefendPhysical { get; set; }
-        public int AttackPower { get; set; }
-        public int DefendPower { get; set; }
-        public int PhysicalCritProb { get; set; }
-        public int MagicCritProb { get; set; }
-        public int ReduceCritProb { get; set; }
-        public int PhysicalCritDamage { get; set; }
-        public int MagicCritDamage { get; set; }
-        public int ReduceCritDamage { get; set; }
-        public int MoveSpeed { get; set; }
-        public int RolePopularity { get; set; }
-        public int ValueHide { get; set; }
-        public int GongfaLearnSpeed { get; set; }
-        public int MishuLearnSpeed { get; set; }
-        public int ExpLevelUp { get; set; }
-    }
-}
+//namespace Runner
+//{
+//    [Serializable]
+//   public class RoleStatusDatas
+//    {
+//        public int LevelID { get; set; }
+//        public int NextLevelID { get; set; }
+//        public int FreeAttributes { get; set; }
+//        public int RoleHP { get; set; }
+//        public int RoleMP { get; set; }
+//        public int RoleSoul { get; set; }
+//        public int BestBlood { get; set; }
+//        public int AttackSpeed { get; set; }
+//        public int AttackPhysical { get; set; }
+//        public int DefendPhysical { get; set; }
+//        public int AttackPower { get; set; }
+//        public int DefendPower { get; set; }
+//        public int PhysicalCritProb { get; set; }
+//        public int MagicCritProb { get; set; }
+//        public int ReduceCritProb { get; set; }
+//        public int PhysicalCritDamage { get; set; }
+//        public int MagicCritDamage { get; set; }
+//        public int ReduceCritDamage { get; set; }
+//        public int MoveSpeed { get; set; }
+//        public int RolePopularity { get; set; }
+//        public int ValueHide { get; set; }
+//        public int GongfaLearnSpeed { get; set; }
+//        public int MishuLearnSpeed { get; set; }
+//        public int ExpLevelUp { get; set; }
+//    }
+//}
